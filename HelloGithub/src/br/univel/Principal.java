baf66fce36709f1af98608c695f4d5d6ec5989ce
clmakihara <clmakihara@gmail.com>
2017-03-09 19:01:59 -0300

    versao 3.0

--- HelloGithub/src/br/univel/Principal.java
+++ HelloGithub/src/br/univel/Principal.java
@@ -5,7 +5,8 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String a = "ola github2";
-		System.out.println("a");
+		String b =a;
+		System.out.println("b");
 
 	}
 
